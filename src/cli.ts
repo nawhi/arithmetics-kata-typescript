618e41493abf8f720e3ec10736bb41ccfb289daf
nawhi <25394965+nawhi@users.noreply.github.com>
2021-04-10 18:53:12 +0100

    Rewrite CLI not to use classes

--- src/cli.ts
+++ src/cli.ts
@@ -2,42 +2,29 @@ import { evaluate } from "./adder";
 import readline from "readline";
 
 export async function runCLI() {
-  const console = new Console();
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
   while (true) {
     try {
-      const input = await console.readLine();
-      console.writeLine(evaluate(input).toString());
+      const input = await readLine(rl);
+      writeLine(rl, evaluate(input));
     } catch (e) {
       return;
     }
   }
 }
 
-class Quit extends Error {}
+function readLine(rl: readline.Interface): Promise<string> {
+  return new Promise((resolve, reject) => {
+    rl.prompt();
+    rl.on("close", () => reject("Finished"));
+    rl.on("line", (input) => resolve(input));
+  });
+}
 
-class Console {
-  private interface: readline.Interface;
-
-  constructor() {
-    this.interface = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-  }
-
-  readLine(): Promise<string> {
-    return new Promise((resolve, reject) => {
-      this.interface.prompt();
-      this.interface.on("close", () => reject(new Quit()));
-      this.interface.on("line", (input) => resolve(input));
-    });
-  }
-
-  writeLine(output: string) {
-    this.interface.write(output + "\n");
-  }
-
-  close(): void {
-    this.interface.close();
-  }
+function writeLine(rl: readline.Interface, line: string): void {
+  rl.write(line + "\n");
 }
