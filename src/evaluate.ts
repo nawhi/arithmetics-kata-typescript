37f1ee5e0c9e91209f7ef60a3143a341833f29ff
nawhi <25394965+nawhi@users.noreply.github.com>
2021-04-17 10:13:01 +0100

    Create a Tokenizer class

--- src/evaluate.ts
+++ src/evaluate.ts
@@ -2,26 +2,41 @@ import { badSyntax } from "./errors";
 import { operate } from "./operations";
 
 export function evaluate(input: string): number {
-  const tokens = input.split(" ");
+  const tokens = new Tokenizer(input);
   return evaluateBinaryOperation(tokens);
 }
 
-function next(tokens: string[]): string {
-  return tokens.shift() || badSyntax();
+function next(tokens: Tokenizer): string {
+  return tokens.next() || badSyntax();
 }
 
 function isNumber(token: string): boolean {
   return /^\d+$/.test(token);
 }
 
+class Tokenizer {
+  private tokens: string[];
+  constructor(rawString: string) {
+    this.tokens = rawString.split(" ");
+  }
+
+  next(): string {
+    return this.tokens.shift() || badSyntax();
+  }
+
+  finished(): boolean {
+    return this.tokens.length === 0;
+  }
+}
+
 function evaluateBinaryOperation(
-  tokens: string[],
+  tokens: Tokenizer,
   accumulator: number = 0
 ): number {
   const ltoken = next(tokens);
   const lvalue =
     ltoken === "(" ? evaluateBinaryOperation(tokens) : Number(ltoken);
-  if (!tokens.length) return lvalue;
+  if (tokens.finished()) return lvalue;
   const op = next(tokens);
   const rvalue = next(tokens);
   const _closingBracket = next(tokens);
