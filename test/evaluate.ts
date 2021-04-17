ec5f468012f29440caccff14b1a8ddb19775ae0c
nawhi <25394965+nawhi@users.noreply.github.com>
2021-04-17 09:58:20 +0100

    Remove unnecessary nested describes

--- test/evaluate.ts
+++ test/evaluate.ts
@@ -1,41 +1,36 @@
-import {expect} from "chai";
-import {evaluate} from "../src/evaluate";
-import {BAD_SYNTAX, DIVISION_BY_ZERO} from "../src/errors";
+import { expect } from "chai";
+import { evaluate } from "../src/evaluate";
+import { BAD_SYNTAX, DIVISION_BY_ZERO } from "../src/errors";
 
 describe("adder", () => {
-  describe("additions", () => {
-    const cases = [
-      ["( 0 + 0 )", 0],
-      ["( 1 + 1 )", 2],
-      ["( 3 + 8 )", 11],
-      ["( 100 + 200 )", 300],
+  const cases = [
+    ["( 0 + 0 )", 0],
+    ["( 1 + 1 )", 2],
+    ["( 3 + 8 )", 11],
+    ["( 100 + 200 )", 300],
 
-      ["( 5 - 3 )", 2],
-      ["( 500 - 930 )", -430],
+    ["( 5 - 3 )", 2],
+    ["( 500 - 930 )", -430],
 
-      ["( 8 * 4 )", 32],
-      ["( 9 * 0 )", 0],
+    ["( 8 * 4 )", 32],
+    ["( 9 * 0 )", 0],
 
+    ["( 10 / 5 )", 2],
+    ["( 5 / 2 )", 2.5],
 
-      ["( 10 / 5 )", 2],
-      ["( 5 / 2 )", 2.5],
-
-      // ["( ( 1 + 2 ) + 3 )", 6], // TODO
-    ] as const;
-    cases.forEach(([input, expected]) => {
-      it(`evaluates ${input} = ${expected}`, () => {
-        expect(evaluate(input)).to.eql(expected);
-      });
+    // ["( ( 1 + 2 ) + 3 )", 6], // TODO
+  ] as const;
+  cases.forEach(([input, expected]) => {
+    it(`evaluates ${input} = ${expected}`, () => {
+      expect(evaluate(input)).to.eql(expected);
     });
   });
 
-  describe("errors", () => {
-    it('throws a syntax error when receiving invalid input', () => {
-      expect(() => evaluate("( 1 % 3 )")).to.throw(BAD_SYNTAX);
-    });
+  it("throws a syntax error when receiving invalid input", () => {
+    expect(() => evaluate("( 1 % 3 )")).to.throw(BAD_SYNTAX);
+  });
 
-    it('throws a division by zero error when asked to divide by zero', () => {
-      expect(() => evaluate("( 9 / 0 )")).to.throw(DIVISION_BY_ZERO);
-    });
+  it("throws a division by zero error when asked to divide by zero", () => {
+    expect(() => evaluate("( 9 / 0 )")).to.throw(DIVISION_BY_ZERO);
   });
 });
