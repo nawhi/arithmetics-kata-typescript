9827fbe45da33612695dd37bd8adffa55a938b4a
nawhi <25394965+nawhi@users.noreply.github.com>
2021-04-19 10:19:02 +0100

    Reorganise operations and tokens source files

--- src/operations.ts
+++ src/operations.ts
@@ -1,4 +1,13 @@
-import {BINARY_OPERATIONS, Operator} from "./tokens";
+import {Operator} from "./tokens";
+import {divisionByZero} from "./errors";
+
+type BinaryOperation = (a: number, b: number) => number;
+export const BINARY_OPERATIONS: { [o in Operator]: BinaryOperation } = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => (b == 0 ? divisionByZero() : a / b),
+};
 
 export function operate(a: number, b: number, operand: Operator): number {
   return BINARY_OPERATIONS[operand](a, b);
--- src/tokens.ts
+++ src/tokens.ts
@@ -1,32 +1,11 @@
-import {divisionByZero} from "./errors";
+export const OPEN_PAREN = "(";
+export const CLOSE_PAREN = ")";
 
 const OPERATORS = ["+", "-", "*", "/"];
 export type Operator = typeof OPERATORS[number];
 
-export function isOperator(token: string): token is Operator {
-  return OPERATORS.includes(token);
-}
-
-type BinaryOperation = (a: number, b: number) => number;
-export const BINARY_OPERATIONS: { [o in Operator]: BinaryOperation } = {
-  "+": (a, b) => a + b,
-  "-": (a, b) => a - b,
-  "*": (a, b) => a * b,
-  "/": (a, b) => (b == 0 ? divisionByZero() : a / b),
-};
-export const OPEN_PAREN = "(";
-export const CLOSE_PAREN = ")";
-
-function isNumber(token: string): boolean {
-  return /^\d+$/.test(token);
-}
-
-export function parseNumber(token: string) {
-  if (!isNumber(token)) {
-    throw new Error(`invalid token: expected a number but got '${token}'`);
-  }
-  return Number(token);
-}
+export const isOperator = (token: string): token is Operator =>
+  OPERATORS.includes(token);
 
 export function parseOperator(token: string): Operator {
   if (!isOperator(token)) {
@@ -35,4 +14,13 @@ export function parseOperator(token: string): Operator {
     );
   }
   return token;
+}
+
+const isNumber = (token: string): boolean => /^\d+$/.test(token);
+
+export function parseNumber(token: string) {
+  if (!isNumber(token)) {
+    throw new Error(`invalid token: expected a number but got '${token}'`);
+  }
+  return Number(token);
 }
